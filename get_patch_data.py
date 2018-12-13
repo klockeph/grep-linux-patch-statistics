960e4cdfb7fe07f9998ee2fb3ac687082422d40b
Philipp Klocke <Phil_K97@gmx.de>
2018-12-13 13:18:13 +0100

    get_patch_data: flags to filter by SHA and start count at 0
    Filtering by SHA is interesting if you employ additional tools like PIT,
    to find out which commits are relevant to your specific config. To do
    this, create a file containing all SHAs to consider (line-by-line) and
    supply it via the "-s" flag.
    
    In general if you want to plot the total number of patches (by not
    specifying a grep filter) you probably want to let them start at zero
    which is now possible by supplying the "-0" flag.

--- get_patch_data.py
+++ get_patch_data.py
@@ -28,23 +28,51 @@ def get_timestamp(version):
     return int(timestamp)
 
 
-def get_commits_single(version, grep_filter):
-    commits = check_output(["git", "log", version, "--grep", grep_filter, "--oneline", "--no-merges"], stderr=STDOUT).decode('utf-8').split('\n')[:-1]
-    return get_timestamp(version), len(commits)
+def count_commits_by_shas(commits, sha_lookup):
+    commits_filtered = 0
+    for c in commits:
+        if c.split(" ")[0] in sha_lookup:
+            commits_filtered += 1
+    return commits_filtered
 
 
-def get_commits_diff(version, previous, grep_filter):
-    commits = check_output(["git", "log", previous+".."+version, "--grep", grep_filter, "--oneline"], stderr=STDOUT).decode('utf-8').split('\n')[:-1]
-    return get_timestamp(version), len(commits)
+def get_commits_single(version, grep_filter, sha_lookup):
+    command = ["git", "log", version, "--format=format:%H", "--no-merges"]
+    if grep_filter is not None:
+        command = command + ["--grep", grep_filter]
+
+    commits = check_output(command, stderr=STDOUT).decode('utf-8').split('\n')[:-1]
+    time = get_timestamp(version)
+    if sha_lookup is None:
+        return time, len(commits)
+    else:
+        return time, count_commits_by_shas(commits, sha_lookup)
 
 
-def get_commits_ordered(versions, grep_filter):
+def get_commits_diff(version, previous, grep_filter, sha_lookup):
+    command = ["git", "log", previous+".."+version, "--format=format:%H", "--no-merges"]
+    if grep_filter is not None:
+        command = command + ["--grep", grep_filter]
+
+    commits = check_output(command, stderr=STDOUT).decode('utf-8').split('\n')[:-1]
+    time = get_timestamp(version)
+    if sha_lookup is None:
+        return time, len(commits)
+    else:
+        return time, count_commits_by_shas(commits, sha_lookup)
+
+
+def get_commits_ordered(versions, grep_filter, sha_lookup, start_zero):
     """Note that this method only works if versions are ordered and build on each other"""
     numbers = {}
-    prev = versions[0], get_commits_single(versions[0], grep_filter)
+    start_time, start_commits = get_commits_single(versions[0], grep_filter, sha_lookup)
+    if start_zero:
+        prev = (versions[0], (start_time, 0))
+    else:
+        prev = (versions[0], (start_time, start_commits))
     numbers[versions[0]] = prev[1]
     for v in versions[1:]:
-        date, n = get_commits_diff(v, prev[0], grep_filter)
+        date, n = get_commits_diff(v, prev[0], grep_filter, sha_lookup)
         prev = v, (date, n + prev[1][1])
         numbers[v] = prev[1]
 
@@ -91,37 +119,60 @@ def write_to_file(filename, numbers):
             writer.writerow([key, value[0], value[1]])
 
 
-def get_commits_dot_zero(grep_filter):
+def get_commits_dot_zero(grep_filter, sha_lookup, start_zero):
     return get_commits_ordered(sorted(
                 get_dot_zero_versions(),
                 key=functools.cmp_to_key(kernel_version_comparator)),
-            grep_filter)
+            grep_filter, sha_lookup, start_zero)
 
 
-def get_commits_lts(version, grep_filter):
+def get_commits_lts(version, grep_filter, sha_lookup, start_zero):
     versions = sorted(
             [v for v in get_versions() if v == version or v.startswith(version+".")],
             key=functools.cmp_to_key(kernel_version_comparator))
 
-    return get_commits_ordered(versions, grep_filter)
+    return get_commits_ordered(versions, grep_filter, sha_lookup, start_zero)
 
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--filter", default="syzkaller.appspotmail.com")
-    parser.add_argument("-p", "--prefix", default="syzkaller")
+    parser.add_argument("-f", "--filter")
+    parser.add_argument("-p", "--prefix", default="patch_data")
+    parser.add_argument("-v", "--version", help="Aggregate stats for a single (LTS) version")
+    parser.add_argument("-s", "--sha_file", help="File containing SHAs (one per line). All other SHAs will be ignored")
+    parser.add_argument("-0", "--start_zero", help="Ignore commits before given version, by setting start to zero", action='store_true')
     args = parser.parse_args()
 
     print("prefix:", args.prefix)
     print("filter:", args.filter)
-    with open("lts_versions") as f:
-        lts_versions = [v.strip() for v in f.read().split(" ")]
 
-    write_to_file(args.prefix + "_zero.csv", get_commits_dot_zero(args.filter))
-    for v in lts_versions:
-        print(v)
-        write_to_file(args.prefix + "_" + v + ".csv", get_commits_lts(v, args.filter))
+    sha_lookup = None
+
+    if args.sha_file:
+        # generate lookup for SHAs
+        sha_lookup = {}
+        with open(args.sha_file, "r") as f:
+            for sha in f.readlines():
+                sha = sha.strip()
+                if len(sha) > 0:
+                    sha_lookup[sha] = True
+
+    if args.version:
+        # generate stats for args.version until linux-<args.version>.y
+        if not args.version.startswith('v'):
+            print("Version has to be a valid git-tag (e.g. 'v4.1')")
+            exit(-1)
+        write_to_file(args.prefix + "_" + args.version + "_filtered.csv", get_commits_lts(args.version, args.filter, sha_lookup, args.start_zero))
+    else:
+        # generate stats for base-versions and LTS-versions
+        with open("lts_versions") as f:
+            lts_versions = [v.strip() for v in f.read().split(" ")]
+
+        write_to_file(args.prefix + "_zero.csv", get_commits_dot_zero(args.filter, sha_lookup, args.start_zero))
+        for v in lts_versions:
+            print(v)
+            write_to_file(args.prefix + "_" + v + ".csv", get_commits_lts(v, args.filter, sha_lookup, args.start_zero))
 
 
 if __name__ == "__main__":
